33b9f041ac688603c1cdda6954a6c0360523ede7
Hudson88 <dmitryboz21@yandex.com>
2023-07-02 18:11:09 +0300

    half

--- build/js/main.js
+++ build/js/main.js
@@ -53,7 +53,7 @@ function setAspectRatioClass() {
       //657 / 1 366 = 0,48096632503
    }
 
-   console.log(ratio);
+   //console.log(ratio);
    if (ratio > 200) {
       $('body').addClass('body-ratio-type--mob-1');
       //0
@@ -78,6 +78,9 @@ function setAspectRatioClass() {
 $(document).ready(function () {
    // $("input[name='phone']").mask(" +7 (999) 999-99-99");
    //$('body').on('click',function(){
+   //  });
+
+
    setTimeout(() => {
       $('.animated-bg__art').addClass('animated-bg__art--activated');
       $('.animated-bg__text-container').addClass('animated-bg__text-container--activated');
@@ -93,7 +96,14 @@ $(document).ready(function () {
       }, 1400);
    }, 600);
 
-   //  });
+   /*$('.animated-bg__art').addClass('animated-bg__art--activated');
+   $('.animated-bg__text-container').addClass('animated-bg__text-container--activated');
+
+   $('.main-content').addClass('main-content--active');
+
+   $('.sidebar').addClass('sidebar--active');
+
+   $('.main-block').addClass('main-block--active');*/
 
 
 
@@ -117,10 +127,96 @@ $(document).ready(function () {
    });
 
 
-   $('.sh-burger').click(function () {
+   $('.sh-burger').on('click', function () {
       $(this).toggleClass('sh-burger--active');
       $('.sidebar').toggleClass('sidebar--mob-opened');
    });
 
 
+   $('body').on('click', '.js-geog-map-point', function () {
+      console.log('click');
+      var href = $(this).attr('data-href');
+      var anchor = '';
+      loadPage(href, function () {
+         setTimeout(() => {
+            if (href.includes('?')) {
+               anchor = href.match(/#(.+?)[\?]/)[1];
+            }
+            else {
+               anchor = href.match(/#(.*)/)[1];
+            }
+            var el = $('#' + anchor);
+
+            console.log(anchor);
+            console.log(el.length);
+
+            if (el.length > 0) {
+               $('.main-block__inner .simplebar-content-wrapper').stop(true).animate({ scrollTop: (el.offset().top - $('.main-block__inner').offset().top - 15) + "px" }, 500);
+               //.scrollTop(el.offset().top - $('.main-block__inner').offset().top, 1000);
+            }
+         }, 300);
+
+      });
+      /*
+      console.log('href');
+      var href=$(this).attr('data-href');
+      var anchor = document.createElement('a');
+      anchor.classList.add('js-change-page');
+      anchor.href = href;
+      //anchor.target="_blank";
+      $(anchor).trigger('click');*/
+      //  $(anchor).remove();
+
+   });
+
+
+   $('body').on('click', '.js-change-page', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      var url = $(this).attr('href');
+      console.log(url);
+      loadPage(url, function () { });
+   });
+
+   function loadPage(url, callbackF) {
+      var prefix_for_links = '';
+
+      if (document.location.href.indexOf('github.io') !== -1) {
+         prefix_for_links = '/build';
+      }
+
+
+      $.ajax({
+         url: prefix_for_links + url,
+         method: 'GET',
+         dataType: 'html',
+         //data: {text: 'Текст'},
+         beforeSend: function () {
+            $('.main-block').addClass('main-block--ajax-loading');
+         },
+         success: function (data, status, jqXHR) {
+
+            var mainBlockInner = $(data).find('.js-main-block-scroll');
+            setTimeout(() => {
+               $('.main-block').html(mainBlockInner);
+               $('.js-main-block-scroll').each(function () {
+                  new SimpleBar($(this)[0], { autoHide: false });
+               });
+               $('.main-block').removeClass('main-block--ajax-loading');
+               callbackF();
+            }, 500);
+            console.log(data);
+            console.log(status);
+            console.log(jqXHR);
+
+         },
+         error: function (data, status, err) {
+            console.log(data);
+            console.log(status);
+            console.log(err);
+         }
+
+      });
+   }
 });
